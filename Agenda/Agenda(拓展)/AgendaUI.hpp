633a31a72c122122564b8d352855a95c8f014a31
LiuYT <31623337+Liu-YT@users.noreply.github.com>
2018-05-07 20:18:47 +0800

    little change

--- Agenda/Agenda(拓展)/AgendaUI.hpp
+++ Agenda/Agenda(拓展)/AgendaUI.hpp
@@ -64,7 +64,7 @@ public:
         new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
         tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
         c=getchar();
-            //------  restore old settings ---------
+        //------  restore old settings ---------
         res=tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);assert(res==0);
         return c;
 	}
